b386fcb5559f345ad00b85992171004d0ca1ff2e
FernandaBianco <123734451+FernandaBianco@users.noreply.github.com>
2023-01-27 08:17:30 -0300

    initial commit

---
+++ works.js
@@ -0,0 +1,9 @@
+
+document.getElementById(`boton`).onclick= function() {
+    console.log("Capturamos el evento click");
+    document.getElementById("demo").innerHTML=" Dios te bendiga"
+}
+
+
+
+// Hice este trabajo con mucho cariño espero este correcto.
